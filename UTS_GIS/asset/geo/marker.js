e7678dbde628a2d9aa7eb114851291d49a3da097
isadurrifki <129967262+isadurrifki@users.noreply.github.com>
2024-11-11 16:46:17 +0700

    Add files via upload

---
+++ UTS_GIS/asset/geo/marker.js
@@ -0,0 +1,523 @@
+var markers = [
+    {
+        'kategori': 'kantor_camat',
+        'icon': getIcon('logo_camat.png'),
+        'data': [
+            {
+                'name': 'Kantor Camat Lembar',
+                'latlang': [-8.716859655478748, 116.08230821751584]
+            },
+            {
+                'name': 'Kantor Camat Sekotong',
+                'latlang': [-8.773867297018363, 116.04952089656535]
+            },
+            {
+                'name': 'Kantor Camat Gerung',
+                'latlang': [-8.679867898003218, 116.1295150942269]
+            },
+            {
+                'name': 'Kantor Camat Kuripan',
+                'latlang': [-8.66815873644333, 116.1695121951856]
+
+            },
+            {
+                'name': 'Kantor Camat Kediri',
+                'latlang': [-8.634868304713763, 116.14998513639763]
+
+            },
+            {
+                'name': 'Kantor Camat Batu Layar',
+                'latlang': [-8.525036965101426, 116.06737344194126]
+
+            },
+            {
+                'name': 'Kantor Camat Gunung Sari',
+                'latlang': [-8.540202576658457, 116.10084950897178]
+
+            },
+            {
+                'name': 'Kantor Camat Narmada',
+                'latlang': [-8.590176421917354, 116.20320491680336]
+
+            },
+            {
+                'name': 'Kantor Camat Labuapi',
+                'latlang': [-8.635434825644307, 116.12355503270088]
+
+            },
+            {
+                'name': 'Kantor Camat Lingsar',
+                'latlang': [-8.57323657510488, 116.18249323824497]
+
+            },
+           
+        ]
+    },
+    {
+        'kategori': 'keamanan',
+        'icon': getIcon('logo_polisi.png'),
+        'data': [
+            {
+                'name': 'Polsek Labu Api Police Station',
+                'latlang': [-8.638691218636277, 116.10234618029631]
+            },
+            {
+                'name': 'Polsek Gerung',
+                'latlang': [-8.681541542175244, 116.1248338189941]
+            },
+            {
+                'name': 'Polsek Lembar Resort Lombok Barat',
+                'latlang': [-8.729784867941799, 116.08633786481208]
+            },
+            {
+                'name': 'Polsek Narmada',
+                'latlang': [-8.593405645689982, 116.19739276333027]
+            },
+            {
+                'name': 'Polsek kediri',
+                'latlang': [-8.635724863709125, 116.14995717852157]
+            },
+            {
+                'name': 'Polsek Sekotong',
+                'latlang': [-8.778210534378855, 116.05080525913496]
+            },
+            {
+                'name': 'Polsek Kuripan',
+                'latlang': [-8.671558573764493, 116.16909472290448]
+            },
+            {
+                'name': 'Polsek Lingsar',
+                'latlang': [-8.567872473092887, 116.15272206338372]
+            },
+            {
+                'name': 'Polsek Gunungsari',
+                'latlang': [-8.53963991354604, 116.0993066094091]
+            },
+            {
+                'name': 'Polsek Batulayar/senggigi',
+                'latlang': [-8.497217038018386, 116.04428779945445]
+            },
+
+        ]
+    },
+    {
+        'kategori': 'kesehatan',
+        'icon': getIcon('logo_rumahsakit.png'),
+        'data': [
+            {
+                'name': 'Puskesmas Lingsar',
+                'latlang': [-8.567287723475824, 116.20005057605519]
+            },
+            {
+                'name': 'Puskesmas Pembantu Lingsar',
+                'latlang': [-8.568169055146022, 116.18222501349781]
+            },
+            {
+                'name': 'Puskesmas Sigerongan',
+                'latlang': [-8.563982710588354, 116.15303565597218]
+            },
+            {
+                'name': 'Rumah Sakit Umum Daerah Patut Patuh Patju',
+                'latlang': [-8.67699777531967, 116.12852550710969]
+            },
+            {
+                'name': 'Puskesmas Kediri',
+                'latlang': [-8.635164998257375, 116.15020253570053]
+            },
+            {
+                'name': 'Puskesmas sekotong',
+                'latlang': [-8.778297015495024, 116.04999625118658]
+            },
+            {
+                'name': 'Puskesmas Lembar',
+                'latlang': [-8.711597284023835, 116.10526959922271]
+            },
+            {
+                'name': 'Puskesmas Labuapi',
+                'latlang': [-8.6274961,116.1225833, 116.12365875358776]
+            },
+            {
+                'name': 'Puskesmas Narmada',
+                'latlang': [-8.604361549091907, 116.17318394619394]
+            },
+            {
+                'name': 'Puskesmas Gunungsari',
+                'latlang': [-8.540006793982592, 116.10056182844784]
+            },
+            {
+                'name': 'Puskesmas meninting',
+                'latlang': [ -8.539199279280744, 116.07769667927114]
+            },
+            {
+                'name': 'Puskesmas pembantu sandik',
+                'latlang': [ -8.536737774213446, 116.09254538757808]
+            },
+   
+
+
+         
+        ]
+    },
+    {
+        'kategori': 'pendidikan',
+        'icon': getIcon('logo_sekolah.png'),
+        'data': [
+            {
+                'name': 'SD Negeri 1 Sekotong Tengah',
+                'latlang': [-8.77564140007199, 116.04985967374027]
+            },
+            {
+                'name': 'SMPN 1 SEKOTONG',
+                'latlang': [-8.77741061281111, 116.04806949469086]
+            },
+            {
+                'name': 'Yayasan Darul Habib Bintaur',
+                'latlang': [-8.837205042670412, 116.09640420200277]
+            },
+            {
+                'name': 'SDI TARBIATUL ULUM',
+                'latlang': [-8.752110064241315, 116.08387298404018]
+
+            },
+            {
+                'name': 'SMP Negeri 1 Kediri',
+                'latlang': [-8.635215382543768, 116.14856198401003]
+            },
+            {
+                'name': 'SMA Kediri',
+                'latlang': [-8.636329138679065, 116.14911988361222]
+
+            },
+            {
+                'name': 'SDN 1 Kediri',
+                'latlang': [-8.635247204204186, 116.14983871558951]
+
+            },
+            {
+                'name': 'SDI TARBIATUL ULUM',
+                'latlang': [-8.752110064241315, 116.08387298404018]
+
+            },
+            {
+                'name': 'SMA NEGERI 3 LEMBAR',
+                'latlang': [-8.765652867977593, 116.0993385766769]
+
+            },
+            {
+                'name': 'SMP NEGERI 5 GERUNG',
+                'latlang': [-8.695885308253489, 116.12969843418325]
+
+            },
+            {
+                'name': 'SMA NEGERI 1 GERUNG',
+                'latlang': [-8.679784577737687, 116.12430959614933]
+
+            },
+            {
+                'name': 'SMAN NEGERI 2 GERUNG',
+                'latlang': [-8.678371026013213, 116.1256459615472]
+
+            },
+            {
+                'name': 'SMKN 1 GERUNG',
+                'latlang': [-8.684735888377956, 116.13873874606658]
+
+            },
+            {
+                'name': 'Sdn 2 babussalam gerung lobar',
+                'latlang': [-8.680528167172266, 116.15061923582448]
+
+            },
+            {
+                'name': 'SMP IT DARUL WAHDAH GERUNG LOMBOK BARAT',
+                'latlang': [-8.686493530102052, 116.15199317001206]
+
+            },
+            {
+                'name': 'SDN 2 Kuripan Utara',
+                'latlang': [-8.654583174345744, 116.17439007832701]
+
+            },
+            {
+                'name': 'SMKN 1 Kuripan',
+                'latlang': [-8.670379848780879, 116.15760188086837]
+
+            },
+            {
+                'name': 'sdn 1 kuripan',
+                'latlang': [-8.67258593735314, 116.1707339754982]
+
+            },
+            {
+                'name': 'SDN 4 Kuripan',
+                'latlang': [-8.666708882706674, 116.17802202470031]
+
+            },
+            {
+                'name': 'SMP Negeri 3 Labuapi',
+                'latlang': [-8.614469084987586, 116.16793155243143]
+
+            },
+            {
+                'name': 'SMAN 2 LABUAPI',
+                'latlang': [-8.610056214866384, 116.16466998608445]
+
+            },
+            {
+                'name': 'SDN 1 Labuapi',
+                'latlang': [-8.6270778,116.1256975, 116.12624373188528]
+
+            },
+            {
+                'name': 'SMA Negeri 1 Labuapi',
+                'latlang': [-8.6406652,116.1057924, 116.11382936251054]
+
+            },
+            {
+                'name': 'SMKN 1 LABUAPI',
+                'latlang': [-8.6278563,116.1204321, 116.12067466869625]
+
+            },
+            {
+                'name': 'SMK Negeri 1 Narmada',
+                'latlang': [-8.592111554648243, 116.22152422046605]
+
+            },
+            {
+                'name': 'SMAN 1 NARMADA',
+                'latlang': [-8.5832852420553, 116.19405840023671]
+
+            },
+            {
+                'name': 'SMKN 2 Lingsar',
+                'latlang': [-8.55648241746884, 116.12727081274392]
+
+            },
+            {
+                'name': 'SMK Negeri 1 Gunungsari',
+                'latlang': [-8.553246530197804, 116.10852598682759]
+
+            },
+            {
+                'name': 'SMPN 2 Gunungsari',
+                'latlang': [-8.549172456448666, 116.13856672763725]
+
+            },
+            {
+                'name': 'SD SMP Negeri Satu Atap 1 Gunungsari',
+                'latlang': [-8.525745687338551, 116.11504911935516]
+
+            },
+            {
+                'name': 'SDN 2 Gunungsari',
+                'latlang': [-8.532664257533307, 116.11024134527712]
+
+            },
+            {
+                'name': 'SMPN 1 BATULAYAR',
+                'latlang': [-8.529183400700699, 116.06328248643065]
+
+            },
+            {
+                'name': 'SDN 2 BATULAYAR',
+                'latlang': [-8.500662173913012, 116.09194993542057]
+
+            },
+            {
+                'name': 'SMK Negeri Batulayar',
+                'latlang': [-8.501171500159797, 116.06242418017443]
+
+            },
+        ]
+    }, 
+    {
+        'kategori': 'pariwisata',
+        'icon': getIcon('logo_wisata.png'),
+        'data': [
+            {
+                'name': 'Ekowisata Mangrove Bagek Kembar',
+                'latlang': [-8.76526905347956, 116.05223875907218]
+            },
+            {
+                'name': 'Mangrove Sekotong Tengah',
+                'latlang': [-8.76315921595951, 116.0467437196394]
+
+            },
+            {
+                'name': 'Pantai Goa Landak',
+                'latlang': [-8.71289731290401, 116.04744572986759]
+
+            },
+            {
+                'name': 'Pantai Elak Elak',
+                'latlang': [-8.724699494267126, 115.97619185156313]
+
+            },
+            {
+                'name': 'Pantai Cemare Lembar',
+                'latlang': [-8.724305095956932, 116.06232926894297]
+
+            },
+            {
+                'name': 'Taman Kota Gerung',
+                'latlang': [-8.674422837817591, 116.16293114859158]
+
+            },
+            {
+                'name': 'Grituk hill and camp',
+                'latlang': [-8.689387999807899, 116.17002400904015]
+
+            },
+            {
+                'name': 'Wisata Kuliner Kebon Ayu',
+                'latlang': [-8.6868015758071, 116.08667148839571]
+
+            },
+            {
+                'name': 'Wisata Aiq Kokoq Babak Kediri',
+                'latlang': [-8.6318488,116.1529958, 116.15228195976547]
+
+            },
+            {
+                'name': 'Taman Agro Wisata Labuapi',
+                'latlang': [-8.6283105,116.1185313, 116.11794834924683]
+
+            },
+            {
+                'name': 'Air Terjun Tibu Remeneng',
+                'latlang': [-8.58460014103574, 116.21865826642787]
+
+            },
+            {
+                'name': 'TWA SURANADI',
+                'latlang': [-8.567120123101448, 116.23917698810749]
+
+            },
+            {
+                'name': 'Wisata Bunut ngengkang',
+                'latlang': [-8.532469856804404, 116.2525930750451]
+
+            },
+            {
+                'name': 'Wisata Tibu Atas',
+                'latlang': [-8.533745751534834, 116.20011805279069]
+
+            },
+            {
+                'name': 'Wisata Alam Aiknyet',
+                'latlang': [ -8.531102779746883, 116.22975793145277]
+
+            },
+            {
+                'name': 'Kolam pemandian',
+                'latlang': [-8.545586729286665, 116.15910243048997]
+
+            },
+            {
+                'name': 'Air Terjun Tibu Tereng',
+                'latlang': [-8.49928178686952, 116.13199970026196]
+
+            }, {
+                'name': 'Air Terjun Temburun Nanas',
+                'latlang': [-8.501490524373095, 116.15943039833128]
+
+            }, {
+                'name': 'Bukit Korea Dopang',
+                'latlang': [-8.519287557155572, 116.13492268202891]
+
+            }, {
+                'name': 'Bukit Tembere',
+                'latlang': [-8.521660432564243, 116.09944647654555]
+
+            },
+            {
+                'name': 'Pantai Batu Bolong',
+                'latlang': [-8.501224554638965, 116.06011747889652]
+
+            },
+            {
+                'name': 'Objek Wisata Batu Bolong',
+                'latlang': [-8.506487552096312, 116.06269240004995]
+
+            },
+            {
+                'name': 'Taman Wisata Alam Kerandangan',
+                'latlang': [-8.475886356122368, 116.07358327837565]
+
+            },
+            {
+                'name': 'Pantai Alang-Alang',
+                'latlang': [-8.478242100642332, 116.03183199880958]
+
+            },
+            {
+                'name': 'Wisata perbukitan sunset',
+                'latlang': [-8.772571204936805, 116.14558264598008]
+
+            },
+            {
+                'name': 'Bukit Simba',
+                'latlang': [-8.816256237950212, 115.94044503564183]
+
+            },
+            {
+                'name': 'Gili Goleng',
+                'latlang': [-8.73775601437664, 115.89140540907499]
+
+            },
+            {
+                'name': 'Siung Sekotong Beach',
+                'latlang': [-8.75140942024173, 115.88035423116459]
+
+            },
+            {
+                'name': 'Orong Bukal',
+                'latlang': [-8.86334837735886, 115.97843348429828]
+
+            },
+            {
+                'name': 'Pantai Meang',
+                'latlang': [-8.872220134734539, 116.05717317156092]
+
+            },
+            {
+                'name': 'Buwun Mas Hills (Bukit Buwun Mas)',
+                'latlang': [-8.83058926639827, 116.04404989121207]
+
+            },
+            {
+                'name': 'Pantai Pengantap',
+                'latlang': [-8.8733392,116.0837498, 116.11588258123065]
+
+            },
+            {
+                'name': 'Wisata perbukitan sunset',
+                'latlang': [-8.773936443194701, 116.10414069873885]
+
+            },
+            {
+                'name': 'Tawun Harbour, sekotong barat',
+                'latlang': [-8.74401986160985, 116.00928419384698]
+
+            },
+            {
+                'name': 'Gili Goleng',
+                'latlang': [-8.738538237742631, 115.9304071396155]
+
+            },
+
+        ] 
+    }
+];
+
+
+function getIcon(icon) {
+    var greenIcon = L.icon({
+        iconUrl: 'asset/img/' + icon,
+        iconSize: [30, 30], // size of the icon
+        iconAnchor: [22, 44], // point of the icon which will correspond to marker's location
+        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+    return greenIcon;
+}
